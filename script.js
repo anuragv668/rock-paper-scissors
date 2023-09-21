875b5814679511ac13e75152610591c0d8eb0a86
anuragv668 <anuragvarma668@gmail.com>
2023-09-21 12:22:23 +0530

    complete the script or logic

---
+++ script.js
@@ -0,0 +1,32 @@
+let getComputerChoice = function () {
+  let num = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  let choice;
+  if (num == 1) {
+    choice = 'Rock';
+  } else if (num == 2) {
+    choice = 'Paper';
+  } else {
+    choice = 'Scissors';
+  }
+  return choice;
+};
+
+let playround = function (playerSelection, computerSelection) {
+  let playerS = playerSelection.toLowerCase();
+  let computerS = computerSelection.toLowerCase();
+  let result;
+
+  if (playerS == computerS) {
+    result = 'It\'s a Draw';
+  } else {
+    if (playerS == 'rock' && computerS == 'scissors' || playerS == 'paper' && computerS == 'rock' || playerS == 'scissors' && computerS == 'paper') {
+      result = 'You Win!';
+    }
+    else {
+      result = 'You Lose!';
+    }
+  }
+
+  return result;
+};
+
